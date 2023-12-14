d3e6a17457db22d5384f9d6f018b7fb3e1871dc2
kasthurisinghe <kasthurisinghekdpk@gmail.com>
2023-12-14 11:47:43 +0530

    Create Customer form loaded to the main menu

--- src/main/java/lk/ijse/controller/CustomerDetails.java
+++ src/main/java/lk/ijse/controller/CustomerDetails.java
@@ -1,7 +1,13 @@
 package lk.ijse.controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import lk.ijse.dto.CreateCustomerDto;
+import lk.ijse.modle.CreateCustomerModle;
+
+import java.sql.SQLException;
 
 public class CustomerDetails {
     public Button createCustomerBtn;
@@ -9,8 +15,30 @@ public class CustomerDetails {
     public Button findCustomerbtn;
     public Button updateCustomerbtn;
     public Button deleteCustomerbtn;
+    public TextField txtCustomerName;
+    public TextField txtCustomerMobile;
+    public TextField txtCustomerAddress;
+    public TextField txtCustomerNic;
+    public TextField txtCstId;
 
-    public void btnCreateCustomerClickOnAction(ActionEvent actionEvent) {
+    public void btnCreateCustomerClickOnAction(ActionEvent actionEvent) throws SQLException {
+        try {
+            String custId=txtCstId.getText();
+            String custName=txtCustomerName.getText();
+            String custAddr=txtCustomerAddress.getText();
+            String custNic=txtCustomerNic.getText();
+            String custMobile=txtCustomerMobile.getText();
+
+            CreateCustomerDto createCustomerDto=new CreateCustomerDto(custId,custName,custAddr,custNic,custMobile);
+            boolean isSaved= CreateCustomerModle.saveCustomer(createCustomerDto);
+
+            if (isSaved){
+                new Alert(Alert.AlertType.CONFIRMATION,"Customer Details saved.").show();
+            }
+        } catch (SQLException e) {
+            new Alert(Alert.AlertType.ERROR,e.getMessage()).show();
+        }
+
     }
 
     public void btnFindCustomerClickOnAction(ActionEvent actionEvent) {
@@ -23,5 +51,8 @@ public class CustomerDetails {
     }
 
     public void btnMainMenuClickOnAction(ActionEvent actionEvent) {
+    }
+
+    public void txtIdClickOnAction(ActionEvent actionEvent) {
     }
 }
--- src/main/java/lk/ijse/controller/MainMenu.java
+++ src/main/java/lk/ijse/controller/MainMenu.java
@@ -2,14 +2,31 @@ package lk.ijse.controller;
 
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainMenu {
     public Button rentalBtn;
     public Button cutomerBtn;
     public Button vehicleBtn;
+    public AnchorPane rootNode1;
 
     public void btnExitClickOnAction(ActionEvent actionEvent) {
         Platform.exit();
+    }
+
+    public void btnClickOnCreateCustomerOnAction(ActionEvent actionEvent) throws IOException {
+        Parent rootnode = FXMLLoader.load(this.getClass().getResource("/view/customerDetails.fxml"));
+        this.rootNode1.getChildren().clear();
+        this.rootNode1.getChildren().add(rootnode);
+    }
+
+    public void btnVehicleRegistryClickOnAction(ActionEvent actionEvent) {
     }
 }
